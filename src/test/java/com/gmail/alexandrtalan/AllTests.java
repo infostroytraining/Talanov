348756ed5a2d9ba56ff947492ac0f7a4f0546b3a
alex <alexandrtalan@gmail.com>
2015-12-07 22:32:53 +0200

    deleted project

--- src/test/java/com/gmail/alexandrtalan/AllTests.java
+++
@@ -1,9 +0,0 @@
-package com.gmail.alexandrtalan;
-
-import org.junit.runner.RunWith;
-import org.junit.runners.Suite;
-
-@Suite.SuiteClasses({MathUtilsTest.class, TextUtilsTest.class, TriangleUtilsTest.class})
-@RunWith(Suite.class)
-public class AllTests {
-}
