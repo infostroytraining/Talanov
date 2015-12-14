cd88ae854e212843c7e3c5673093759c211a00e2
alex <alexandrtalan@gmail.com>
2015-12-14 19:58:18 +0200

    delete project

--- ServletExample/src/main/java/com/gmail/alexandrtalan/dao/UserDAO.java
+++
@@ -1,7 +0,0 @@
-package com.gmail.alexandrtalan.dao;
-
-
-import com.gmail.alexandrtalan.entity.User;
-
-public interface UserDAO extends DAO<User> {
-}
--- ServletExample/src/test/java/com/gmail/alexandrtalan/AllTests.java
+++
@@ -1,9 +0,0 @@
-package com.gmail.alexandrtalan;
-
-import org.junit.runner.RunWith;
-import org.junit.runners.Suite;
-
-@Suite.SuiteClasses({})
-@RunWith(Suite.class)
-public class AllTests {
-}
